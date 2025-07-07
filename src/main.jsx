43aac3f57b98d07aca619e145c7ed45cef714daa
ruhanaatiq <ruhana.atiq@gmail.com>
2025-07-07 20:53:54 +0600

    contexts added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from "react-router";
+import { router } from './router/router.jsx';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import AuthProvider from './contexts/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Initialize AOS animation library
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Optional: animation duration in ms
+      once: true,     // Optional: animation happens only once
+    });
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/router/router.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ]
+  }
+]);
